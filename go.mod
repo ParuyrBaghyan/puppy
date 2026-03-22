39cba427145363e79197eb7d550e20e5187d2167
ParuyrBaghyan <paruyr.baghyan@mail.ru>
2026-03-22 20:52:47 +0400

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ParuyrBaghyan/puppy
 
 go 1.24.5
+
+require github.com/ParuyrBaghyan/dog v1.2.1
--- puppy.go
+++ puppy.go
@@ -1,10 +1,15 @@
 package puppy
 
-func Bark() string{
+import "github.com/ParuyrBaghyan/dog"
+
+func Bark() string {
 	return "Woof!"
 }
 
-func  Barks() string {
+func Barks() string {
 	return "Woof! Woof! Woof!"
 }
 
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
+}
